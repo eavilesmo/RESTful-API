5f0c3af8fc5b2951b19b9854d8e6d2ec74995f33
E. Macarena Avilés <72768790+eavilesmo@users.noreply.github.com>
2022-05-25 19:23:44 +0200

    Add base files

---
+++ app.py
@@ -0,0 +1,66 @@
+# -------------------------
+# IMPORTS
+# -------------------------
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import RadioField
+
+# Creating an instance of Flask
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "22_this_is_a_flask_test_22"
+
+# -------------------------
+# VARIABLES
+# -------------------------
+radio_bt = [("Option 1", "Create a new space ship")]
+redirect_message = """<h2>Welcome to FTC!</h2><button type="button" onclick="window.location.href='/home'">Home</button>"""
+
+# -------------------------
+# CLASSES
+# -------------------------
+class SpaceShip():
+    def __init__(self, ship_list:list):
+        self.health = 10
+        self.is_alive = True
+        self.id = len(ship_list)
+        ship_list.append(self)
+
+    def check_status(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+
+class Game():
+    ship_list = []
+
+class HomePage(FlaskForm):
+    radio_buttons = RadioField(choices = radio_bt)
+
+# -------------------------
+# FUNCTIONS
+# -------------------------
+
+# Creating welcome page
+@app.route("/", methods=["GET", "POST"])
+def welcome_page():
+    return redirect_message
+
+# Creating home page
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    form = HomePage()
+    if form.validate_on_submit():
+        print(form.validate_on_submit())
+        if form.radio_buttons.data == "Option 1":
+            print(form.radio_buttons.data)
+            SpaceShip(Game.ship_list)
+            message_created = "The space ship has been created!"
+            return render_template("home.html", form=form, message = message_created)
+    return render_template("home.html", form=form)
+
+# -------------------------
+# MAIN
+# -------------------------
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host='0.0.0.0')
---
+++ test_app.py
@@ -0,0 +1,36 @@
+# Imports
+import app
+import unittest
+
+# Classes
+class FlaskTestApp(unittest.TestCase):
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+        self.app.application.config['WTF_CSRF_ENABLED'] = False
+
+    # Testing if flask is initialized correctly
+    def test_01_app(self):
+        response = self.app.get("/", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+    
+    # Index Page
+    # Testing if the message from the index page is correctly created
+    def test_02_index(self):
+        response = self.app.get("/", content_type="html/text")
+        self.assertTrue(b"Welcome to FTC!" in response.data)
+    
+    # Home Page
+    # Testing if the options from the home page are correctly created
+    def test_03_home(self):
+        response = self.app.get("/home", content_type="html/text")
+        self.assertTrue(b"Create a new space ship" in response.data)
+
+    # Testing if the first option is correctly working
+    def test_04_home_create_ship(self):
+        response = self.app.post("/home", data=dict(radio_buttons="Option 1"), follow_redirects=True)
+        self.assertIn(b"The space ship has been created!", response.data)
+
+# Main
+if __name__ == "__main__":
+    unittest.main()
